cc17aba1cac55ab36e0db2b45e13b99afacd8f24
euclid <euclid@euclid--MacBook-Pro.local>
2024-07-20 22:20:11 +0400

    modify beam search

--- beam_search.py
+++ beam_search.py
@@ -35,42 +35,43 @@ class BeamSearch:
     def get_unique_states(
         self, 
         states: torch.Tensor,
-        gen_indexes: torch.Tensor
+        candidate_solutions: torch.Tensor
     ) -> torch.Tensor:
         hashed = torch.sum(self.hash_vec * states, dim=1)
         hashed_sorted, idx = torch.sort(hashed)
         mask = torch.cat((torch.tensor([True]), hashed_sorted[1:] - hashed_sorted[:-1] > 0))
 
-        gen_indexes = gen_indexes[:, idx[mask]]
-        return states[idx][mask], gen_indexes
+        candidate_solutions = candidate_solutions[:, idx[mask]]
+        return states[idx[mask]], candidate_solutions
 
     def get_neighbors(
         self, 
         states: torch.Tensor,
-        parent_actions: torch.Tensor
+        candidate_solutions: torch.Tensor
     ) -> torch.Tensor:
         expanded_states = states.unsqueeze(1).expand(states.size(0), self.n_gens, self.state_size)
-        expand_parent_actions = parent_actions.unsqueeze(2).expand(
-            parent_actions.size(0), 
-            parent_actions.size(1),
+        
+        expand_candidate_solutions = candidate_solutions.unsqueeze(2) # (path_of_action, different_solutions, 1)
+        
+        expand_candidate_solutions = expand_candidate_solutions.expand(
+            candidate_solutions.size(0), 
+            candidate_solutions.size(1),
             self.n_gens
-        )
-        # print("before expand_parent_actions:", expand_parent_actions.shape)
-        expand_parent_actions = expand_parent_actions.reshape(parent_actions.size(0), parent_actions.size(1) * self.n_gens)
-        # print("after expand_parent_actions:", expand_parent_actions.shape)
+        ) # (path of action, different solutions, ngens=12)
+        
+        expand_candidate_solutions = expand_candidate_solutions.reshape(
+            candidate_solutions.size(0), 
+            candidate_solutions.size(1) * self.n_gens
+        ) # (path_of_action, different_solutions * ngens=12)
 
         indexes = self.generators.unsqueeze(0).expand(states.size(0), self.n_gens, self.state_size)        
-        gen_indexes = torch.arange(0, self.n_gens, dtype=torch.int64).expand(states.size(0), self.n_gens)
         new_states = torch.gather(input=expanded_states, dim=2, index=indexes)
 
-        gen_indexes = gen_indexes.reshape(1, gen_indexes.shape[0] * gen_indexes.shape[1])
-        # print("gen_indexes before cat:", gen_indexes.shape)
-        gen_indexes = torch.cat([expand_parent_actions, gen_indexes])
-        # print("gen_indexes after:", gen_indexes.shape)
+        applied_actions = torch.arange(0, self.n_gens, dtype=torch.int64).expand(states.size(0), self.n_gens)
+        applied_actions = applied_actions.reshape(1, applied_actions.shape[0] * applied_actions.shape[1])
+        candidate_solutions = torch.cat([expand_candidate_solutions, applied_actions]) 
 
-        # exit()
-
-        return new_states, gen_indexes
+        return new_states, candidate_solutions
 
     def batch_predict(
         self, 
@@ -109,24 +110,23 @@ class BeamSearch:
 
     def do_greedy_step(
         self, 
-        states: torch.Tensor, 
-        B: int = 1000
+        states: torch.Tensor,
+        candidate_solutions: torch.Tensor, 
+        beam_width: int = 1000
     ) -> torch.Tensor:
-        # print("parent_actions:", self.parent_actions.shape)
-        neighbors, gen_indexes = self.get_neighbors(states, self.parent_actions)
+        neighbors, candidate_solutions = self.get_neighbors(states, candidate_solutions)
         
         neighbors = neighbors.flatten(end_dim=1)
-        # gen_indexes = gen_indexes.flatten(end_dim=1)
         
-        neighbors, gen_indexes = self.get_unique_states(neighbors, gen_indexes)
-        # print("final gen_indexes:", gen_indexes.shape)
+        neighbors, candidate_solutions = self.get_unique_states(neighbors, candidate_solutions)
         
         y_pred = self.predict_clipped_values(neighbors)
-        idx = torch.argsort(y_pred)[:B]
+        idx = torch.argsort(y_pred)[:beam_width]
 
-        self.parent_actions = gen_indexes[:, idx[:B]]
+        candidate_solutions = candidate_solutions[:, idx]
+        neighbors = neighbors[idx]
 
-        return neighbors[idx]
+        return neighbors, candidate_solutions
 
     def search(
         self,
@@ -135,8 +135,8 @@ class BeamSearch:
         if len(state.shape) < 2:
             state = state.unsqueeze(0)
 
-        self.parent_actions = torch.full(
-            size=[1, 1],
+        candidate_solutions = torch.full(
+            size=[1, 1], # (path of action, different solutions)
             fill_value=-1,
             dtype=torch.int64
         )
@@ -144,28 +144,26 @@ class BeamSearch:
         self.model.eval()
         states = state.clone()
         for j in range(self.num_steps):
-            states = self.do_greedy_step(states, self.beam_width)
-            has_goal = (states == self.goal_state).all(dim=1)
-            search_result = has_goal.nonzero(as_tuple=True)[0]
-            # print("has goal:", tuple_goal)
-            # if (states == self.goal_state).all(dim=1).any():
+            states, candidate_solutions = self.do_greedy_step(
+                states=states, 
+                candidate_solutions=candidate_solutions,
+                beam_width=self.beam_width,
+            )
+            search_result = (states == self.goal_state).all(dim=1).nonzero(as_tuple=True)[0]
             if (len(search_result) > 0):
-                # print("len(search_result)", len(search_result))
                 solution_index = search_result.item()
-                solution = self.parent_actions[:, solution_index]
-                print("solution_index:", solution_index)
-                print("solution:", solution)                
+                solution = candidate_solutions[:, solution_index]
                 return solution[1:]
         return None
 
 if __name__ == "__main__":
     deepcube_test = open_pickle("./assets/data/deepcubea/data_0.pkl")
-    i = 0
+    i = 1
     state = torch.tensor(deepcube_test['states'][i], dtype=torch.int64).unsqueeze(0)
     solution = deepcube_test['solutions'][i]    
 
     print("state:", state.shape)
-    print("solution len:", len(solution))
+    print("solution_len (optimal):", len(solution))
 
     model = Pilgrim()
     model.load_state_dict(torch.load("./assets/models/Cube3ResnetModel.pt"))
@@ -188,13 +186,13 @@ if __name__ == "__main__":
     end = time.time()
     duration = np.round(end - start, 3)
 
-    print("found_len:", solution)
+    print("solution_len:", len(solution))
     print("duration:", duration)
     
     print("state", state.shape)    
     for a in solution:
         state = state[:, generators[a]]
     
-    print(solution)
-    print(state)
+    print("solution:", solution)
+    print("result_state:", state)
     print("state == goal_state:", (state == goal_state).all(dim=1))
