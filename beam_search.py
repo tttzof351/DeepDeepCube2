96e8951ac0854b8f93f1f5bddf89682fbcfb665b
euclid <euclid@euclid--MacBook-Pro.local>
2024-07-20 21:58:20 +0400

    Add return solution

--- beam_search.py
+++ beam_search.py
@@ -34,22 +34,43 @@ class BeamSearch:
 
     def get_unique_states(
         self, 
-        states: torch.Tensor
+        states: torch.Tensor,
+        gen_indexes: torch.Tensor
     ) -> torch.Tensor:
         hashed = torch.sum(self.hash_vec * states, dim=1)
         hashed_sorted, idx = torch.sort(hashed)
         mask = torch.cat((torch.tensor([True]), hashed_sorted[1:] - hashed_sorted[:-1] > 0))
-        return states[idx][mask]
+
+        gen_indexes = gen_indexes[:, idx[mask]]
+        return states[idx][mask], gen_indexes
 
     def get_neighbors(
         self, 
-        states: torch.Tensor
+        states: torch.Tensor,
+        parent_actions: torch.Tensor
     ) -> torch.Tensor:
-        return torch.gather(
-            states.unsqueeze(1).expand(states.size(0), self.n_gens, self.state_size), 
-            2, 
-            self.generators.unsqueeze(0).expand(states.size(0), self.n_gens, self.state_size)
+        expanded_states = states.unsqueeze(1).expand(states.size(0), self.n_gens, self.state_size)
+        expand_parent_actions = parent_actions.unsqueeze(2).expand(
+            parent_actions.size(0), 
+            parent_actions.size(1),
+            self.n_gens
         )
+        # print("before expand_parent_actions:", expand_parent_actions.shape)
+        expand_parent_actions = expand_parent_actions.reshape(parent_actions.size(0), parent_actions.size(1) * self.n_gens)
+        # print("after expand_parent_actions:", expand_parent_actions.shape)
+
+        indexes = self.generators.unsqueeze(0).expand(states.size(0), self.n_gens, self.state_size)        
+        gen_indexes = torch.arange(0, self.n_gens, dtype=torch.int64).expand(states.size(0), self.n_gens)
+        new_states = torch.gather(input=expanded_states, dim=2, index=indexes)
+
+        gen_indexes = gen_indexes.reshape(1, gen_indexes.shape[0] * gen_indexes.shape[1])
+        # print("gen_indexes before cat:", gen_indexes.shape)
+        gen_indexes = torch.cat([expand_parent_actions, gen_indexes])
+        # print("gen_indexes after:", gen_indexes.shape)
+
+        # exit()
+
+        return new_states, gen_indexes
 
     def batch_predict(
         self, 
@@ -91,10 +112,20 @@ class BeamSearch:
         states: torch.Tensor, 
         B: int = 1000
     ) -> torch.Tensor:
-        neighbors = self.get_neighbors(states).flatten(end_dim=1)
-        neighbors = self.get_unique_states(neighbors)
+        # print("parent_actions:", self.parent_actions.shape)
+        neighbors, gen_indexes = self.get_neighbors(states, self.parent_actions)
+        
+        neighbors = neighbors.flatten(end_dim=1)
+        # gen_indexes = gen_indexes.flatten(end_dim=1)
+        
+        neighbors, gen_indexes = self.get_unique_states(neighbors, gen_indexes)
+        # print("final gen_indexes:", gen_indexes.shape)
+        
         y_pred = self.predict_clipped_values(neighbors)
         idx = torch.argsort(y_pred)[:B]
+
+        self.parent_actions = gen_indexes[:, idx[:B]]
+
         return neighbors[idx]
 
     def search(
@@ -104,13 +135,28 @@ class BeamSearch:
         if len(state.shape) < 2:
             state = state.unsqueeze(0)
 
+        self.parent_actions = torch.full(
+            size=[1, 1],
+            fill_value=-1,
+            dtype=torch.int64
+        )
+
         self.model.eval()
         states = state.clone()
         for j in range(self.num_steps):
             states = self.do_greedy_step(states, self.beam_width)
-            if (states == self.goal_state).all(dim=1).any():
-                return j
-        return -1
+            has_goal = (states == self.goal_state).all(dim=1)
+            search_result = has_goal.nonzero(as_tuple=True)[0]
+            # print("has goal:", tuple_goal)
+            # if (states == self.goal_state).all(dim=1).any():
+            if (len(search_result) > 0):
+                # print("len(search_result)", len(search_result))
+                solution_index = search_result.item()
+                solution = self.parent_actions[:, solution_index]
+                print("solution_index:", solution_index)
+                print("solution:", solution)                
+                return solution[1:]
+        return None
 
 if __name__ == "__main__":
     deepcube_test = open_pickle("./assets/data/deepcubea/data_0.pkl")
@@ -138,9 +184,17 @@ if __name__ == "__main__":
         goal_state=goal_state,
         device = "mps"
     )
-    found_len = beam_search.search(state=state)
+    solution = beam_search.search(state=state)
     end = time.time()
     duration = np.round(end - start, 3)
 
-    print("found_len:", found_len)
+    print("found_len:", solution)
     print("duration:", duration)
+    
+    print("state", state.shape)    
+    for a in solution:
+        state = state[:, generators[a]]
+    
+    print(solution)
+    print(state)
+    print("state == goal_state:", (state == goal_state).all(dim=1))
