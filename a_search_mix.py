e4531e865e6a9ea5d0e1ad2806db48f2db6749ef
euclid <euclid@euclid--MacBook-Pro.local>
2024-08-21 11:29:31 +0400

    Add find best temperature function

--- a_search_mix.py
+++ a_search_mix.py
@@ -398,6 +398,69 @@ def sample_solutions():
         print("Element of path!: ", action)
         path.append(action)
 
+def find_best_temperature():
+    set_seed(0)
+    deepcube_test = open_pickle("./assets/data/deepcubea/data_0.pkl")
+    game = Cube3Game("./assets/envs/qtm_cube3.pickle")
+    generators = torch.tensor(game.actions, dtype=torch.int64)
+
+    device = "cpu"
+    model_device = "cpu"
+
+    model = Pilgrim(
+        input_dim = 54, 
+        hidden_dim1 = 5000, 
+        hidden_dim2 = 1000, 
+        num_residual_blocks = 4 
+    ) # ~14M
+
+    model.load_state_dict(
+        torch.load(
+            "./assets/models/Cube3ResnetModel_value_policy_3_8B_14M.pt",
+            map_location=model_device)
+    )
+    model = model.to(model_device)
+    goal_state = torch.arange(0, 54, dtype=torch.int64)
+
+    TS = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    BS = [128, 256, 512, 1024, 2048, 4096, 8192]
+
+    for b in BS:
+        mean_lens = []
+        for t in TS:
+            our_lens = []
+            with TimeContext(f"b={b}; t={t}] Execution time:", True):
+                for i in tqdm(range(0, 10)):
+                    state = torch.tensor(deepcube_test['states'][i], dtype=torch.int64)#.unsqueeze(0)
+
+                    state = state.unsqueeze(0)
+
+                    path_finder = AStarVector(
+                        model=model,
+                        generators=generators,
+                        num_steps=10_000,
+                        b_exp=b,
+                        b_keep=0,
+                        temperature=t,
+                        goal_state=goal_state,
+                        verbose=False,
+                        device=device,
+                        model_device=model_device
+                    )
+                    
+                    # opt_solution = deepcube_test['solutions'][i]
+
+                    solution, processed_count = path_finder.search(state)
+                    our_lens.append(len(solution))        
+            mean_lens.append(np.round(np.mean(our_lens), 4))
+        best_id = np.argsort(mean_lens)[0]
+        best_t = TS[best_id]
+        best_mean = mean_lens[best_id]
+        print(f"b={b}; means={mean_lens}; best_mean={best_mean}, best_t={best_t}")
+
+
+
 if __name__ == "__main__": 
-    test_a_star()
+    # test_a_star()
     # sample_solutions()
+    find_best_temperature()
